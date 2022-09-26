8d359055f1d7e59350061ed3ad4f4e2a57252260
fahadaltaf <fahadaltaf10SE49@hotmail.com>
2022-09-27 00:56:09 +0400

    initial commit

---
+++ Models.cs
@@ -0,0 +1,125 @@
+﻿using System;
+namespace caahep.org_scraper
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
+    public class AddOnTrack
+    {
+        public int addOnTrackId { get; set; }
+        public string title { get; set; }
+    }
+
+    public class AwardLetter
+    {
+        public int documentId { get; set; }
+        public string title { get; set; }
+        public string fileUrl { get; set; }
+        public string fileDownloadName { get; set; }
+    }
+
+    public class ProgramDirector
+    {
+        public int peopleId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string address2 { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zip { get; set; }
+        public string country { get; set; }
+        public string phone { get; set; }
+        public string extension { get; set; }
+        public string title { get; set; }
+        public string email { get; set; }
+        public bool active { get; set; }
+        public double? lng { get; set; }
+        public double? lat { get; set; }
+    }
+
+    public class Root
+    {
+        public int id { get; set; }
+        public int professionId { get; set; }
+        public List<AddOnTrack> addOnTracks { get; set; }
+        public string concentrationTitle { get; set; }
+        public string name { get; set; }
+        public string institutionName { get; set; }
+        public string institutionWebsite { get; set; }
+        public bool degreeDiploma { get; set; }
+        public bool degreeCertificate { get; set; }
+        public bool degreeAssociate { get; set; }
+        public bool degreeBaccalaureate { get; set; }
+        public bool degreeMasters { get; set; }
+        public DateTime accreditedOn { get; set; }
+        public int statusId { get; set; }
+        public DateTime? statusEffectiveDate { get; set; }
+        public string programWebsite { get; set; }
+        public string outcomesUrl { get; set; }
+        public bool distance { get; set; }
+        public bool futureVoluntaryWithdraw { get; set; }
+        public ProgramDirector programDirector { get; set; }
+        public AwardLetter awardLetter { get; set; }
+        public List<Satellite> satellites { get; set; }
+    }
+
+    public class Satellite
+    {
+        public int satelliteId { get; set; }
+        public int programId { get; set; }
+        public string title { get; set; }
+        public string state { get; set; }
+        public string city { get; set; }
+    }
+
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Paging
+    {
+        public int pageNumber { get; set; }
+        public int maxPageSize { get; set; }
+        public int pageSize { get; set; }
+    }
+
+    public class Payload
+    {
+        public string searchText { get; set; }
+        public bool isInternational { get; set; }
+        public List<int> professionIds { get; set; } = new List<int>();
+        public List<int> concentrations { get; set; } = new List<int>();
+        public List<int> addOnTracks { get; set; } = new List<int>();
+        public List<int> sortStatuses { get; set; } = new List<int>();
+        public bool degreeDiploma { get; set; }
+        public bool degreeAssociate { get; set; }
+        public bool degreeBaccalaureate { get; set; }
+        public bool degreeCertificate { get; set; }
+        public bool degreeMasters { get; set; }
+        public Paging paging { get; set; }
+    }
+
+
+    public class DataModel
+    {
+        public string ProgramUrl { get; set; }
+        public string InstututionUrl { get; set; }
+        public string OutcomeUrl { get; set; }
+        public string InstitutionName { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string AccredationDate { get; set; }
+        public string Degree { get; set; }
+        public string AwardLetter { get; set; }
+        public string Address { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string County { get; set; }
+        public string Zip { get; set; }
+        public string ProgramDirectorTitle { get; set; }
+        public string ProgramDirectorFirstName { get; set; }
+        public string ProgramDirectorLastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+    }
+
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿
+
+using System.IO;
+using System.Net.Http.Json;
+using System.Text.Json;
+using caahep.org_scraper;
+using OfficeOpenXml;
+
+List<DataModel> entries = new List<DataModel>();
+Console.WriteLine("Please wait downloading data...");
+HttpClient client = new HttpClient();
+var model = JsonSerializer.Deserialize<Payload>(File.ReadAllText("payload.json"));
+var response = await client.PostAsJsonAsync<Payload>("https://search-api.caahep.org/api/programs?code=jTWtFM5JdJXTkfWQKs7aoKS2tDadlFZnSuLT0V1N2CbIr70RiqjabA==", model);
+if (response != null)
+{
+    var result = await response.Content.ReadFromJsonAsync<List<Root>>();
+    if (result != null)
+    {
+        foreach (var item in result)
+        {
+            var entry = new DataModel
+            {
+                ProgramUrl = item.programWebsite,
+                InstututionUrl = item.institutionWebsite,
+                OutcomeUrl = item.outcomesUrl,
+                AccredationDate = item.accreditedOn.ToString("MMM dd, yyyy"),
+                Address = item.programDirector.address,
+                Address2 = item.programDirector.address2,
+                AwardLetter = $"https://search-api.caahep.org/api/9157/download-award-letter/{item.awardLetter.fileUrl}?code=jTWtFM5JdJXTkfWQKs7aoKS2tDadlFZnSuLT0V1N2CbIr70RiqjabA==",
+                City = item.programDirector.city,
+                County = item.programDirector.country,
+                // Degree =item.programDirector.d,
+                Email = item.programDirector.email,
+                InstitutionName = item.institutionName,
+                Name = item.name,
+                Phone = item.programDirector.phone,
+                ProgramDirectorTitle = item.programDirector.title,
+                ProgramDirectorFirstName = item.programDirector.firstName,
+                ProgramDirectorLastName = item.programDirector.lastName,
+                // Status =item.statusId,
+                Zip = item.programDirector.zip
+
+            };
+
+            if (item.degreeAssociate)
+            {
+                entry.Degree = "Associate";
+            }
+            else if (item.degreeBaccalaureate)
+            {
+                entry.Degree = "Baccalaureate";
+            }
+            else if (item.degreeCertificate)
+            {
+                entry.Degree = "Certificate";
+            }
+            else if (item.degreeDiploma)
+            {
+                entry.Degree = "Diploma";
+            }
+            else if (item.degreeMasters)
+            {
+                entry.Degree = "Masters";
+            }
+            entries.Add(entry);
+        }
+    }
+    else
+        Console.WriteLine("Unable to parse the result or no data found");
+}
+else
+    Console.WriteLine("Unable to fetch data from server. Please try again.");
+
+if (entries.Count > 0)
+{
+    ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+    using (var excel = new ExcelPackage())
+    {
+        var time = DateTime.Now.ToString("yyyyMMddHHmmss");
+        var worksheet = excel.Workbook.Worksheets.Add($"Data");
+        worksheet.Cells.LoadFromCollection(entries, true);
+        excel.SaveAs($"{time}.xlsx");
+        Console.WriteLine($"Data has been exported");
+    }
+}
+
+Console.ReadKey();
+Console.WriteLine("Press any key to exit...");
